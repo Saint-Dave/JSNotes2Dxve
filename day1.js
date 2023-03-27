37cc83443acce340d10b8c5d8cd7f1c267b14a88
Saint-Dave <babaloladavid7@gmail.com>
2023-03-27 12:19:16 +0100

    first commit

---
+++ day1.js
@@ -0,0 +1,12 @@
+function generatePassword(length) {
+  const charset = "aeiouPQRST13579&*[)$";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return password;
+}
+
+// Example usage:
+const password = generatePassword(10); // generates a 12-character password
+console.log(password); // outputs a random password
